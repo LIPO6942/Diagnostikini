60041fc727504f45e52dbad63580d1935ec79721
Smilou814 <smilou25@gmail.com>
2025-07-13 09:49:07 +0000

    "Ton diagnostic, en quelques clics" rendre "ton diagnostique " et en des

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center w-full">
       <h1 className="text-3xl font-extrabold text-center text-foreground mb-4">
-        Ton diagnostic, en quelques clics
+        Ton diagnostic <br /> en quelques clics
       </h1>
 
       <SymptomChecker />
