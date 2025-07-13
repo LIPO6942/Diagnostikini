4429c86e237d353e8dc0efd9e51edb447bca8a55
Smilou814 <smilou25@gmail.com>
2025-07-13 09:10:53 +0000

    lorsque j'ouvre l'application sur mon téléphone via le qr code ,  l'appl

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="theme-color" content="#115e59" />
       </head>
       <body className="font-body antialiased">
