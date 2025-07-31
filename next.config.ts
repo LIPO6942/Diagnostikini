e18be8fd37fb5ab0c994ce78c046485c93550cd1
Smilou814 <smilou25@gmail.com>
2025-07-31 07:23:33 +0000

    sauf que quand j 'essaire de l'ouvrir en hors ligne , ca me ressort (vOU

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,19 @@ const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
+  // disable: process.env.NODE_ENV === "development",
+  runtimeCaching: [
+    {
+      urlPattern: /^https?.*/,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'offlineCache',
+        expiration: {
+          maxEntries: 200,
+        },
+      },
+    },
+  ],
 });
 
 const nextConfig: NextConfig = {
