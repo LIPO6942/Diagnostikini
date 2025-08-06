ba39358d63b4e7b438371eccf3050e23daf79e7e
Smilou814 <smilou25@gmail.com>
2025-08-06 22:37:48 +0000

    centrer ces elements

--- src/components/layout/main-layout.tsx
+++ src/components/layout/main-layout.tsx
@@ -62,8 +62,10 @@ export function MainLayout({ children }: { children: React.ReactNode }) {
                 <div />
               </header>
             </TooltipProvider>
-            <main className="p-4 sm:p-6 lg:p-8">
-              {children}
+            <main className="flex flex-1 flex-col items-center p-4 sm:p-6 lg:p-8">
+              <div className="w-full max-w-4xl">
+                {children}
+              </div>
             </main>
           </SidebarInset>
       </SidebarProvider>
