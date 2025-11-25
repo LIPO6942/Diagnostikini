6e018d3d74879ee4e695d32866b5088b0398a0d6
Moslem <moslem5017@example.com>
2025-11-25 17:12:30 +0100

    enrichissement symptome

--- src/constants/profile-options.ts
+++ src/constants/profile-options.ts
@@ -3,39 +3,241 @@
  */
 
 export const medicalHistoryOptions = [
-    "Diabète",
-    "Hypertension artérielle",
-    "Asthme",
-    "Maladies cardiaques",
+    // Maladies métaboliques et endocriniennes
+    "Diabète de type 1",
+    "Diabète de type 2",
     "Hypothyroïdie",
     "Hyperthyroïdie",
-    "Anémie",
-    "Maladies rénales",
+    "Obésité",
+    "Hypercholestérolémie",
+    "Hypertriglycéridémie",
+    "Goutte",
+
+    // Maladies cardiovasculaires
+    "Hypertension artérielle",
+    "Insuffisance cardiaque",
+    "Infarctus du myocarde",
+    "Angine de poitrine",
+    "Arythmie cardiaque",
+    "Fibrillation auriculaire",
+    "Valvulopathie",
+    "Artériopathie",
+    "AVC (Accident Vasculaire Cérébral)",
+    "Phlébite / Thrombose veineuse",
+
+    // Maladies respiratoires
+    "Asthme",
+    "BPCO (Bronchopneumopathie Chronique Obstructive)",
+    "Emphysème",
+    "Bronchite chronique",
+    "Apnée du sommeil",
+    "Tuberculose",
+    "Fibrose pulmonaire",
+
+    // Maladies digestives
+    "Reflux gastro-œsophagien (RGO)",
+    "Ulcère gastrique",
+    "Ulcère duodénal",
+    "Maladie de Crohn",
+    "Rectocolite hémorragique",
+    "Syndrome de l'intestin irritable",
+    "Cirrhose hépatique",
+    "Hépatite B",
+    "Hépatite C",
+    "Pancréatite",
+    "Calculs biliaires",
+
+    // Maladies rénales et urinaires
+    "Insuffisance rénale chronique",
+    "Calculs rénaux",
+    "Infections urinaires récurrentes",
+    "Prostatite",
+    "Hypertrophie bénigne de la prostate",
+
+    // Maladies hématologiques
+    "Anémie ferriprive",
+    "Anémie falciforme",
+    "Thalassémie",
+    "Hémophilie",
+    "Leucémie",
+    "Lymphome",
+
+    // Maladies neurologiques
+    "Épilepsie",
+    "Maladie de Parkinson",
+    "Sclérose en plaques",
+    "Migraine chronique",
+    "Neuropathie",
+    "AVC antérieur",
+
+    // Maladies rhumatologiques
+    "Arthrose",
+    "Polyarthrite rhumatoïde",
+    "Spondylarthrite ankylosante",
+    "Lupus érythémateux",
+    "Fibromyalgie",
+    "Ostéoporose",
+
+    // Maladies psychiatriques
+    "Dépression",
+    "Trouble bipolaire",
+    "Schizophrénie",
+    "Troubles anxieux",
+    "Trouble obsessionnel compulsif (TOC)",
+
+    // Maladies dermatologiques
+    "Psoriasis",
+    "Eczéma",
+    "Vitiligo",
+
+    // Cancers
+    "Cancer du sein",
+    "Cancer du poumon",
+    "Cancer colorectal",
+    "Cancer de la prostate",
+    "Cancer de la peau",
+    "Autre cancer",
+
+    // Maladies infectieuses
+    "VIH/SIDA",
+    "COVID-19 (forme grave)",
+
+    // Autres
+    "Glaucome",
+    "Cataracte",
+    "Dégénérescence maculaire",
 ];
 
 export const allergyOptions = [
+    // Allergies médicamenteuses
     "Pénicilline",
+    "Amoxicilline",
+    "Céphalosporines",
     "Aspirine / AINS",
+    "Ibuprofène",
     "Sulfamides",
+    "Codéine",
+    "Morphine",
     "Produits de contraste iodés",
+    "Anesthésiques locaux",
+    "Latex",
+
+    // Allergies alimentaires
     "Arachides",
-    "Fruits de mer",
+    "Fruits à coque (noix, amandes, etc.)",
+    "Fruits de mer (crustacés)",
+    "Poissons",
+    "Œufs",
+    "Lait / Produits laitiers",
+    "Gluten",
+    "Soja",
+    "Sésame",
+    "Moutarde",
+    "Céleri",
+
+    // Allergies environnementales
     "Pollen",
-    "Poils d'animaux",
+    "Acariens",
+    "Poils d'animaux (chats)",
+    "Poils d'animaux (chiens)",
+    "Moisissures",
+    "Poussière",
+
+    // Autres allergies
+    "Piqûres d'insectes (abeilles, guêpes)",
+    "Nickel",
+    "Parfums",
+    "Produits cosmétiques",
 ];
 
 export const treatmentOptions = [
-    "Antihypertenseurs",
-    "Antidiabétiques",
-    "Hormones thyroïdiennes",
-    "Anticoagulants",
-    "Analgésiques (régulièrement)",
+    // Cardiovasculaire
+    "Antihypertenseurs (IEC)",
+    "Antihypertenseurs (ARA II)",
+    "Bêta-bloquants",
+    "Inhibiteurs calciques",
+    "Diurétiques",
+    "Anticoagulants (AVK - Warfarine)",
+    "Anticoagulants (AOD - Rivaroxaban, Apixaban)",
+    "Antiagrégants plaquettaires (Aspirine)",
+    "Antiagrégants plaquettaires (Clopidogrel)",
+    "Statines (anticholestérol)",
+    "Fibrates",
+
+    // Diabète et métabolisme
+    "Insuline",
+    "Metformine",
+    "Sulfamides hypoglycémiants",
+    "Inhibiteurs DPP-4",
+    "Analogues du GLP-1",
+    "Inhibiteurs SGLT2",
+
+    // Thyroïde
+    "Hormones thyroïdiennes (Lévothyroxine)",
+    "Antithyroïdiens de synthèse",
+
+    // Respiratoire
+    "Bronchodilatateurs (bêta-2 agonistes)",
+    "Corticoïdes inhalés",
+    "Antihistaminiques",
+    "Antileucotriènes",
+
+    // Digestif
+    "Inhibiteurs de la pompe à protons (IPP)",
+    "Anti-H2",
+    "Antiacides",
+    "Laxatifs",
+    "Antidiarrhéiques",
+    "Anti-spasmodiques",
+
+    // Douleur et inflammation
+    "Paracétamol (régulièrement)",
+    "Anti-inflammatoires non stéroïdiens (AINS)",
+    "Opioïdes faibles (Tramadol, Codéine)",
+    "Opioïdes forts (Morphine)",
+    "Corticoïdes oraux",
+
+    // Neurologie et psychiatrie
+    "Antidépresseurs (ISRS)",
+    "Antidépresseurs (tricycliques)",
+    "Anxiolytiques (benzodiazépines)",
+    "Neuroleptiques / Antipsychotiques",
+    "Antiépileptiques",
+    "Antiparkinsoniens",
+    "Traitement de la migraine (triptans)",
+
+    // Rhumatologie
+    "Méthotrexate",
+    "Biothérapies (anti-TNF)",
+    "Corticoïdes",
+    "Bisphosphonates (ostéoporose)",
+
+    // Gynécologie
     "Contraceptifs oraux",
-    "Antidépresseurs",
-    "Anti-inflammatoires",
+    "Traitement hormonal substitutif (THS)",
+
+    // Urologie
+    "Alpha-bloquants (prostate)",
+    "Inhibiteurs de la 5-alpha réductase",
+
+    // Immunosuppresseurs
+    "Immunosuppresseurs",
+    "Corticoïdes au long cours",
+
+    // Autres
+    "Antihistaminiques",
+    "Collyres (glaucome)",
+    "Suppléments vitaminiques",
+    "Suppléments de fer",
+    "Antiviraux",
+    "Antibiotiques (traitement prolongé)",
 ];
 
 export const additionalSymptomOptions = [
+    // Symptômes généraux
+    "Fièvre",
+    "Frissons",
+    "Fatigue intense",
     "Perte d'appétit",
     "Prise de poids",
     "Perte de poids",
@@ -44,6 +246,143 @@ export const additionalSymptomOptions = [
     "Soif excessive",
     "Irritabilité",
     "Gonflement des chevilles",
+    "Malaise général",
+
+    // ORL (Oto-Rhino-Laryngologie)
+    "Mal de gorge",
+    "Enrouement / Voix rauque",
+    "Douleur à l'oreille (otalgie)",
+    "Bourdonnements d'oreille (acouphènes)",
+    "Perte d'audition",
+    "Écoulement nasal",
+    "Nez bouché / Congestion nasale",
+    "Saignement de nez (épistaxis)",
+    "Éternuements fréquents",
+    "Démangeaisons du nez",
+    "Perte de l'odorat (anosmie)",
+    "Diminution de l'odorat (hyposmie)",
+    "Écoulement de l'oreille",
+    "Sensation d'oreille bouchée",
+    "Vertiges",
+    "Difficulté à avaler (dysphagie)",
+    "Ganglions du cou gonflés",
+    "Douleur au niveau des sinus",
+    "Pression faciale",
+
+    // Bucco-dentaire
+    "Mal de dents",
+    "Douleur à la mastication",
+    "Saignement des gencives",
+    "Gencives gonflées",
+    "Gencives rouges",
+    "Mauvaise haleine (halitose)",
+    "Sécheresse buccale (xérostomie)",
+    "Excès de salive",
+    "Aphtes / Ulcères buccaux",
+    "Plaies dans la bouche",
+    "Langue douloureuse",
+    "Langue blanche / Chargée",
+    "Goût métallique",
+    "Perte du goût (agueusie)",
+    "Dents sensibles au chaud/froid",
+    "Dents qui bougent",
+    "Abcès dentaire",
+    "Douleur de la mâchoire",
+    "Craquement de la mâchoire",
+    "Difficulté à ouvrir la bouche",
+
+    // Respiratoire
+    "Toux sèche",
+    "Toux grasse",
+    "Essoufflement au repos",
+    "Essoufflement à l'effort",
+    "Respiration sifflante",
+    "Douleur thoracique à la respiration",
+    "Expectoration / Crachats",
+    "Crachats avec sang",
+    "Oppression thoracique",
+
+    // Cardiovasculaire
+    "Palpitations",
+    "Douleur thoracique",
+    "Battements cardiaques irréguliers",
+    "Gonflement des jambes",
+    "Extrémités froides",
+    "Varices douloureuses",
+
+    // Digestif
+    "Nausées",
+    "Vomissements",
+    "Diarrhée",
+    "Constipation",
+    "Douleur abdominale",
+    "Ballonnements",
+    "Brûlures d'estomac",
+    "Reflux gastrique",
+    "Sang dans les selles",
+    "Selles noires",
+    "Selles décolorées",
+    "Flatulences excessives",
+    "Crampes abdominales",
+
+    // Neurologique
+    "Maux de tête",
+    "Migraines",
+    "Étourdissements",
+    "Troubles de la mémoire",
+    "Difficultés de concentration",
+    "Engourdissements",
+    "Fourmillements",
+    "Tremblements",
+    "Troubles de l'équilibre",
+    "Faiblesse musculaire",
+    "Vision floue",
+    "Vision double",
+    "Sensibilité à la lumière",
+
+    // Dermatologique
+    "Démangeaisons cutanées",
+    "Éruption cutanée",
+    "Rougeurs de la peau",
+    "Peau sèche",
+    "Urticaire",
+    "Plaques sur la peau",
+    "Changement de grain de beauté",
+    "Peau qui pèle",
+
+    // Urinaire
+    "Douleur en urinant",
+    "Urines fréquentes",
+    "Urines urgentes",
+    "Sang dans les urines",
+    "Urines troubles",
+    "Difficulté à uriner",
+    "Incontinence urinaire",
+
+    // Musculo-squelettique
+    "Douleurs articulaires",
+    "Raideur matinale",
+    "Douleurs musculaires",
+    "Douleur au dos",
+    "Douleur au cou",
+    "Gonflement des articulations",
+    "Limitation des mouvements",
+
+    // Gynécologique (le cas échéant)
+    "Règles irrégulières",
+    "Règles abondantes",
+    "Règles douloureuses",
+    "Saignements entre les règles",
+    "Pertes vaginales anormales",
+    "Douleurs pelviennes",
+
+    // Psychologique
+    "Anxiété",
+    "Stress",
+    "Tristesse persistante",
+    "Perte d'intérêt",
+    "Troubles du sommeil",
+    "Cauchemars fréquents",
 ];
 
 export const analysisTypeOptions = [
